9acb296bcf5a8777f424734fd35e6bcbcb8258f2
Daniel Zduniak <dzduniak@gmail.com>
2022-08-08 08:37:06 +0200

    Fix missing annotations.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,11 @@ pub fn set_panic_hook() {
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
     pub fn warn(s: &str);
+    
+    #[wasm_bindgen(js_namespace = console)]
     pub fn error(s: &str);
 }
 
